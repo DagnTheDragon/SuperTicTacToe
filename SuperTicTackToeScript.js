4240d47d69938e912335eb0994025d51dfaf16d3
Evan <evanbotkin448@gmail.com>
2024-03-02 13:24:28 -0500

    fifth commit: conditional game checking

--- SuperTicTackToeScript.js
+++ SuperTicTackToeScript.js
@@ -26,20 +26,54 @@ function PlayGame(){
     let BR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
     
     //Check all sectors for 3 in a row
-    Out_Game[0] = Checker(TL_Game);
-    Out_Game[1] = Checker(TC_Game);
-    Out_Game[2] = Checker(TR_Game);
-    Out_Game[3] = Checker(CL_Game);
-    Out_Game[4] = Checker(CC_Game);
-    Out_Game[5] = Checker(CR_Game);
-    Out_Game[6] = Checker(BL_Game);
-    Out_Game[7] = Checker(BC_Game);
-    Out_Game[8] = Checker(BR_Game);
-    Checker(Out_Game);
+    Out_Game = checks(Out_Game, TL_Game, TC_Game, TR_Game, CL_Game, CC_Game, CR_Game, BL_Game, BC_Game, BR_Game);
+    TL_Game[9] = Out_Game[0];
+    TC_Game[9] = Out_Game[1];
+    TR_Game[9] = Out_Game[2];
+    CL_Game[9] = Out_Game[3];
+    CC_Game[9] = Out_Game[4];
+    CR_Game[9] = Out_Game[5];
+    BL_Game[9] = Out_Game[6];
+    BC_Game[9] = Out_Game[7];
+    BR_Game[9] = Out_Game[8];
+}
+//Runs all the checks
+function checks(Out_Game, TL_Game, TC_Game, TR_Game, CL_Game, CC_Game, CR_Game, BL_Game, BC_Game, BR_Game){
+    if (TL_Game[9] == NaN){
+        Out_Game[0] = checker(TL_Game);
+    }
+    if (TC_Game[9] == NaN){
+        Out_Game[1] = checker(TC_Game);
+    }
+    if (TR_Game[9] == NaN){
+        Out_Game[2] = checker(TR_Game);
+    }
+    if (CL_Game[9] == NaN){
+        Out_Game[3] = checker(CL_Game);
+    }
+    if (CC_Game[9] == NaN){
+        Out_Game[4] = checker(CC_Game);
+    }
+    if (CR_Game[9] == NaN){
+        Out_Game[5] = checker(CR_Game);
+    }
+    if (BL_Game[9] == NaN){
+        Out_Game[6] = checker(BL_Game);
+    }
+    if (BC_Game[9] == NaN){
+        Out_Game[7] = checker(BC_Game);
+    }
+    if (BR_Game[9] == NaN){
+        Out_Game[8] = checker(BR_Game);
+    }
+    if (Out_Game[9] == NaN){
+        Out_Game[9] = checker(Out_Game);
+    }
+    return Out_Game
 }
 
 //Checks if ther are any three in a rows in any of the 10 game boards
-function Checker(arr){
+function checker(arr){
     if (arr[0] != NaN && arr[0] == arr [1] && arr[0] == arr[2]){
         //Top Row Check
         return arr[0];
