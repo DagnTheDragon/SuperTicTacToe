d736975fd26d30b81619e81690db48f7e59dcac6
Evan <evanbotkin448@gmail.com>
2024-03-01 19:47:15 -0500

    third commit: slight update to js

--- SuperTicTackToeScript.js
+++ SuperTicTackToeScript.js
@@ -11,53 +11,61 @@ function PlayGame(){
         6) Bottom Left    (BL)
         7) Bottom Center  (BC)
         8) Bottom Right   (BR)
+        9) Victor Status
     */
 
-    let Out_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let TL_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let TC_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let TR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let CL_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let CC_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let CR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let BL_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let BC_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
-    let BR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let Out_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let TL_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let TC_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let TR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let CL_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let CC_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let CR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let BL_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let BC_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
+    let BR_Game = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
     
     //Check all sectors for 3 in a row
-    Checker(TL_Game);
-    Checker(TC_Game);
-    Checker(TR_Game);
-    Checker(CL_Game);
-    Checker(CC_Game);
-    Checker(CR_Game);
-    Checker(BL_Game);
-    Checker(BC_Game);
-    Checker(BR_Game);
+    Out_Game[0] = Checker(TL_Game);
+    Out_Game[1] = Checker(TC_Game);
+    Out_Game[2] = Checker(TR_Game);
+    Out_Game[3] = Checker(CL_Game);
+    Out_Game[4] = Checker(CC_Game);
+    Out_Game[5] = Checker(CR_Game);
+    Out_Game[6] = Checker(BL_Game);
+    Out_Game[7] = Checker(BC_Game);
+    Out_Game[8] = Checker(BR_Game);
     Checker(Out_Game);
-
 }
 
 //Checks if ther are any three in a rows in any of the 10 game boards
 function Checker(arr){
     if (arr[0] != NaN && arr[0] == arr [1] && arr[0] == arr[2]){
         //Top Row Check
+        return arr[0];
     } else if (arr[3] != NaN && arr[3] == arr [4] && arr[3] == arr[5]){
-        //Center Row Check
+        // Center Row Check
+        return arr[3];
     } else if (arr[6] != NaN && arr[6] == arr [7] && arr[6] == arr[8]){
         //Bottom Row Check
+        return arr[6];
     } else if (arr[0] != NaN && arr[0] == arr [3] && arr[0] == arr[6]){
         //Left Column Check
+        return arr[0];
     } else if (arr[1] != NaN && arr[1] == arr [4] && arr[1] == arr[7]){
         //Center Column Check
+        return arr[1];
     } else if (arr[2] != NaN && arr[2] == arr [5] && arr[2] == arr[8]){
         //Right Column Check
+        return arr[2]
     } else if (arr[0] != NaN && arr[0] == arr [4] && arr[0] == arr[8]){
         //Top Left to Bottom Right Diagonal Check
+        return arr[0];
     } else if (arr[6] != NaN && arr[6] == arr [4] && arr[6] == arr[2]){
         //Bottom Left to Top Right Diagonal Check
+        return arr[6];
     } else {
         //No three in a row yet
-        console.log('Nothing')
+        return NaN
     }
 }
